7533f7baca4b281b756ef939edbbadc267d66c45
javimiguelez <91721699+Xavitheforce@users.noreply.github.com>
2022-03-08 19:16:41 +0000

    ejercicio dicotomia iterativo(compila bien)

--- ejercicios/O_insercionD.py
+++ ejercicios/O_insercionD.py
@@ -1,23 +1,22 @@
-def ordenar_dicotomia(n, m, lista1, lista2):
-  if n < len(lista1):
-    if lista1[n] < lista2[m]:
-      if m-1 < 0 or (lista1[n] > lista2[m-1]):
-        lista2.insert(m, lista1[n])
-      else:
-        ordenar_dicotomia(n, m-1, lista1, lista2)
-    elif lista1[n] > lista2[m]:
-      if m+1 > (len(lista2)-1) or (lista1[n] < lista2[m+1]):
-        lista2.insert(m+1, lista1[n])
-      else:
-        ordenar_dicotomia(n, m+1, lista1, lista2)
-    ordenar_dicotomia(n+1, m, lista1, lista2)
+def ordenar_dicotomia(lista1, lista2):
+  for i in range(1, (len(lista1)-1)):
+    mitad = int((len(lista2)-1)/2)
+    if lista1[i] < lista2[mitad]:
+      for j in range(mitad, -1, -1):
+        if j-1 < 0 or (lista1[i] > lista2[j-1]):
+          lista2.insert(j, lista1[i])
+          break
+    elif lista1[i] > lista2[mitad]:
+      for j in range(mitad, (len(lista2))):
+        if j+1 > (len(lista2)-1) or (lista1[i] < lista2[j+1]):
+          lista2.insert(j+1, lista1[i])
+          break
 
 def iniciar():
   lista = ["medium", "adiós", "3", "bebé", "ola", "guerra", "espectro", "5", "sed", "secta", "código", "confusión", "2", "clara"]
   lista_o = []
   lista_o.append(lista[0])
-  centro = int((len(lista_o)-1)/2)
-  ordenar_dicotomia(1, centro, lista, lista_o)
+  ordenar_dicotomia(lista, lista_o)
   print(sorted(lista))
   print(str(lista_o))
 iniciar()
